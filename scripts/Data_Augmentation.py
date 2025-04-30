1aae5856d96e7ed9f7f673d0b5ec448a8691e72c
MOHAB NOUR EDDIN <148438943+mohab1996@users.noreply.github.com>
2025-04-30 23:39:33 +0200

    Update Data_Augmentation.py

--- scripts/Data_Augmentation.py
+++ scripts/Data_Augmentation.py
@@ -3,6 +3,7 @@ import numpy as np
 import librosa
 import soundfile as sf
 import random
+import colorednoise as cn
 
 
 def add_white_noise(signal, noise_percentage_factor):
@@ -12,6 +13,22 @@ def add_white_noise(signal, noise_percentage_factor):
 
 def time_stretch(signal, sr, time_stretch_rate):
     return librosa.effects.time_stretch(signal, rate=time_stretch_rate)
+
+def pitch_shift_file(y,sr, n_steps):
+    """Shifts the pitch of an audio file by a given number of semitones."""
+
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=n_steps)
+
+    return y_shifted
+
+def add_pink_noise(signal,value):
+    return cn.powerlaw_psd_gaussian(value, len(signal))
+
+def hpss_filter(signal):
+    harmonic, percussive = librosa.effects.hpss(signal)
+    augmented_samples = [harmonic, percussive]
+    augmented_sample=random.choice(augmented_samples)
+    return augmented_sample
 
 def create_and_save_augmented_audio(original_file_name, sr, function_name, augmented_signal, output_folder, count):
     filename = os.path.join(output_folder, f"augmented_{function_name}_{count}_{os.path.basename(original_file_name)}")
@@ -31,15 +48,30 @@ if __name__ == "__main__":
             file_path = os.path.join(input_folder, file)
             try:
                 signal, sr = librosa.load(file_path)
-                if random.random() < 0.5:
-                    augmented_signal_noise = add_white_noise(signal.copy(), 0.1)
+                if random.random() < 0.2:
+                    augmented_signal_noise = add_white_noise(signal.copy(), 0.2)
                     count += 1
-                    create_and_save_augmented_audio(file, sr, "noise", augmented_signal_noise, output_folder,count)
+                    create_and_save_augmented_audio(file, sr, "white_noise_added", augmented_signal_noise, output_folder,count)
         
-                if random.random() < 0.5:
-                    augmented_signal_stretch = time_stretch(signal.copy(), sr, 1.2)
+                if random.random() < 0.2:
+                    augmented_signal_stretch = time_stretch(signal.copy(), sr,time_stretch_rate=0.9)
                     count += 1
-                    create_and_save_augmented_audio(file, sr, "stretch", augmented_signal_stretch, output_folder,count)
+                    create_and_save_augmented_audio(file, sr, "time_stretched", augmented_signal_stretch, output_folder,count)
+
+                if random.random() < 0.2:
+                    augmented_signal_noise = pitch_shift_file(signal.copy(),sr, 6)
+                    count += 1
+                    create_and_save_augmented_audio(file, sr, "pithc_shifted", augmented_signal_noise, output_folder,count)
+
+                if random.random() < 0.2:
+                    augmented_signal_noise = add_pink_noise(signal.copy(), 2)
+                    count += 1
+                    create_and_save_augmented_audio(file, sr, "pink_noise_added", augmented_signal_noise, output_folder,count)
+
+                if random.random() < 0.2:
+                    augmented_signal_noise = hpss_filter(signal.copy())
+                    count += 1
+                    create_and_save_augmented_audio(file, sr, "hpss_filtered", augmented_signal_noise, output_folder,count)
     
             except Exception as e:
                 print(f"Error processing {file}: {e}")
